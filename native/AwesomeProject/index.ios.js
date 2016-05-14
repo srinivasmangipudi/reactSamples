1b7d0c331fbcf896210d0618b5b7499661e2b243
Srinivas Mangipudi <shrinimann@gmail.com>
2016-05-14 13:44:34 +0530

    Rendering first item from server

--- native/AwesomeProject/index.ios.js
+++ native/AwesomeProject/index.ios.js
@@ -4,49 +4,105 @@
  * @flow
  */
 
+ var MOCKED_MOVIES_DATA = [
+   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+ ];
+
+ /**
+  * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+  * their very own API that lives in React Native's Github repo.
+  */
+ var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
 class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
   }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+
+  render() {
+      if (!this.state.movies) {
+        return this.renderLoadingView();
+      }
+
+      var movie = this.state.movies[0];
+      return this.renderMovie(movie);
+    }
+
+    renderLoadingView() {
+      return (
+        <View style={styles.container}>
+          <Text>
+            Loading movies...
+          </Text>
+        </View>
+      );
+    }
+
+    renderMovie(movie) {
+      return (
+        <View style={styles.container}>
+          <Image
+            source={{uri: movie.posters.thumbnail}}
+            style={styles.thumbnail}
+          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+        </View>
+      );
+    }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
