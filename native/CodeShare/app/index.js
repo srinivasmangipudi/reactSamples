20fb0b1faeb1be025d97d33f59201d4dc7230db2
Srinivas Mangipudi <shrinimann@gmail.com>
2016-05-31 20:46:04 +0530

    Firebase connected to android and ios apps and fetching/displaying count.

--- native/CodeShare/app/index.js
+++ native/CodeShare/app/index.js
@@ -1,17 +1,84 @@
 // app/index.js
 
-import React, { Component, View, Text, StyleSheet, Platform } from 'react-native';
+//import React, { AppRegistry, Component, View, Text, StyleSheet,ListView, Platform } from 'react-native';
 import Tabs from 'react-native-tabs';
 import Button from './components/button';
 
+var React = require('react-native');
+var {
+  Component,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput,
+  ListView,
+  Platform
+} = React;
+
+var Firebase = require('firebase');
+var ReactFireMixin = require('reactfire');
+
 export default class CodeShare extends Component {
+
+  mixins(ReactFireMixin) {}
+
   constructor(props) {
     super(props);
 
+    this.awareUsersNowRef = new Firebase("https://project-2569167554904200855.firebaseio.com/awareUsersNow");
+
     this.state = {
-      page: 'first'
+      page: 'Touch to see how many people are Aware!'
     };
   }
+
+  componentWillMount() {
+    //var awareUsersNowRef = new Firebase("https://project-2569167554904200855.firebaseio.com/awareUsersNow");
+
+    //this.bindAsObject(awareUsersNowRef, "awareUsersNowCount");
+    this.awareUsersNowRef.on('value', function(snapshot) {
+      var count = snapshot.val();
+
+      this.setState({ page:  count + ' people are Aware with you!'});
+    }.bind(this));
+
+    /*this.setState({
+      page: 'mounted the horse' + this.awareUsersNowCount.value
+    })*/
+  }
+
+  listenForAwareUsers(awareUsersNowRef) {
+      /*awareUsersNowRef.on('value', (snap) => {
+        // get children as an array
+        var users = snap;
+
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(items)
+        });
+      });*/
+    }
+
+  /*_renderItem(item) {
+    return (
+      <ListItem item="{item}" onpress="{()" ==""> {}} />
+    );
+  }*/
+
+  /*render() {
+    return (
+      <View style="{styles.container}">
+
+        <StatusBar title="Grocery List">
+
+        <ListView datasource="{this.state.dataSource}" renderrow="{this._renderItem.bind(this)}" style="{styles.listview}/">
+
+        <ActionButton title="Add" onpress="{()" ==""> {}} />
+
+      </View>
+    );
+  }*/
 
   render() {
       const { page } = this.state;
@@ -20,14 +87,19 @@ export default class CodeShare extends Component {
 
       return (
         <View style={styles.container}>
+
           <Tabs
             selected={page}
             style={tabbarStyles}
-            selectedStyle={{color:'red'}} onSelect={el=>this.setState({page:el.props.name})}
-          >
-              <Text name="first">First</Text>
-              <Text name="second">Second</Text>
-              <Text name="third">Third</Text>
+            selectedStyle={{color:'red'}} onSelect={el=>this.setState({page:el.props.name})}>
+
+            <Text
+              dataSource={page}
+              name="first">First
+            </Text>
+
+            <Text name="second">Second</Text>
+            <Text name="third">Third</Text>
           </Tabs>
 
           <Text>CodeSharing App</Text>
--- native/CodeShare/index.android.js
+++ native/CodeShare/index.android.js
@@ -2,3 +2,76 @@ import React, { AppRegistry } from 'react-native';
 import CodeShare from './app';
 
 AppRegistry.registerComponent('CodeShare', () => CodeShare);
+
+
+/*'use strict';
+
+var React = require('react-native');
+var Firebase = require('firebase');
+var ReactFireMixin = require('reactfire');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+} = React;
+
+var CodeShare = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState: function() {
+    return {
+      items: []
+    };
+  },
+  componentWillMount: function() {
+    this.ref = new Firebase('https://nanochat.firebaseio.com/chat');
+    this.bindAsArray(this.ref, 'items');
+  },
+  _onPressButton: function() {
+    this.ref.push({ name: 'puf', message: this.state.text });
+  },
+  render: function() {
+    var createItem = function(item, index) {
+      return <Text>{item.name}: {item.message}</Text>
+    };
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        {this.state.items.map(createItem)}
+        <View style={styles.horizontal}>
+          <TextInput onChangeText={(text) =>
+              this.setState({ text: text})} value={this.state.text} />
+          <TouchableHighlight onPress={this._onPressButton}>
+            <Text>Send</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('CodeShare', () => CodeShare);
+*/
